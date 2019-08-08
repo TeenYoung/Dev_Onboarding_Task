e8de1221c714a254778262da2b6307667a2a927a
Tim <teenyoung42@gmail.com>
2019-08-08 21:04:02 +1200

    Add project files.

---
+++ Dev_Onboarding_Task/ClientApp/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import Button from './components/button.js'
+
+
+export default class App extends Component {
+  displayName = App.name
+
+  render() {
+    return (
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetchdata' component={Button} />
+      </div>
+    );
+  }
+}
